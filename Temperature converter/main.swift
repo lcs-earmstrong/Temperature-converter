a401b4f8103bd640c2a9e03a85c20177f20b79dd
Evan Armstrong <earmstrong@lcs.on.ca>
2021-02-02 15:23:25 -0500

    Made conversion from Fahrenheit to Celsius and vice versa.

--- Temperature converter/main.swift
+++ Temperature converter/main.swift
@@ -7,5 +7,32 @@
 
 import Foundation
 
-print("Hello, World!")
+print("what type of temperature measurment would you likke to convert from?")
+print("C: Celcius")
+print("F: Fahrenheit")
+print("Enter your choice (C/F)")
+
+let measurement = readLine()!
+
+print("What tmeperature would you like to convert from?")
+
+let temperature = Double(readLine()!)!
+
+func conversionFromFahrenheitToCelcius(temp: Double) -> Double {
+    return (temperature - 32) * 0.5556
+}
+
+if measurement == "C" {
+    let celciusTemperature = conversionFromFahrenheitToCelcius(temp: temperature)
+    print("The temperature is \(celciusTemperature) degrees")
+}
+
+func conversionFromCelciusToFahrenheit(temp: Double) -> Double {
+    return temperature * 1.8 + 32
+}
+
+if measurement == "F" {
+    let fahrenheitTemperature = conversionFromCelciusToFahrenheit(temp: temperature)
+    print("The temperature is \(fahrenheitTemperature) degrees")
+}
 
